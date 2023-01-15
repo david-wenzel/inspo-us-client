c6508c7b0d048d0d12bfc959102c323856a811eb
David Wenzel <dwenzel95@gmail.com>
2023-01-14 19:43:05 -0600

    add navbar

--- inspo-us/src/App.js
+++ inspo-us/src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
 function App() {
   return (
     <div className="App">
-     
+      <Router>
+      <Navbar />
+      <Switch>
+      <Route exact path="/" component={ Home } />
+      </Switch>
+      </Router>
     </div>
   );
 }
---
+++ inspo-us/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+
+export default function Navbar() {
+  return (
+    <div>
+      <ul>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
+      </ul>
+    </div>
+  )
+}
