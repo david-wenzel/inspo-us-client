6446feb22bea334a930617bdf3a232d8d2f85963
David Wenzel <dwenzel95@gmail.com>
2023-01-15 13:52:36 -0600

    add about component

--- inspo-us/src/App.js
+++ inspo-us/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import UserList from './components/UserList'
+import About from './components/About';
 function App() {
   return (
     <div className="App">
@@ -11,6 +12,7 @@ function App() {
       <Switch>
       <Route exact path="/" component={ Home } />
       <Route exact path="/users" component={ UserList }/>
+      <Route exact path="/about" component={ About } />
       </Switch>
       </Router>
     </div>
