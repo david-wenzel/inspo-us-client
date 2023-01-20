fb7a7fbdec80946b6a599c0b478a1cbb85077822
David Wenzel <dwenzel95@gmail.com>
2023-01-20 14:38:48 -0600

    add show board posts button to show the associated posts witha board in boards list

--- inspo-us/src/components/BoardCard.js
+++ inspo-us/src/components/BoardCard.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 const BoardCard = ({ id, board, deleteBoard }) => {
 // console.log(board)
+const [showPosts, setShowPosts] = useState(false);
+
+const onClick = () => setShowPosts(!showPosts);
+
+
 
 let posts = board.posts
 let postImgs = posts.map(({img_url}) => img_url)
@@ -11,9 +17,13 @@ let postImgs = posts.map(({img_url}) => img_url)
       <NavLink to={`/users/${id}/boards/${board.id}`}>{ board.title }</NavLink> <button onClick={ () => deleteBoard(board.id) }>Delete</button>
       {console.log(postImgs)}
       <br/>
-      {postImgs.map((url, index) => (
+      {/* {postImgs.map((url, index) => (
           <img key={index} src={url} style={{ maxWidth: "200px", maxHeight: "200px"}} alt="image" />
-        ))}
+        ))} */}
+{ !showPosts ? <button onClick={onClick}>{`show ${board.title} posts`}</button> : null }
+      {showPosts ? postImgs.map((url, index) => (
+        <img key={index} src={url} style={{ maxWidth: "200px", maxHeight: "200px"}} alt="image" />
+      )) : null}
     </li>
   )
 }
