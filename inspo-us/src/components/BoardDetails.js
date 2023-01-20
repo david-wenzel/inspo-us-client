630bc24c5cc4b2b7024dfe457c5acd9760470550
David Wenzel <dwenzel95@gmail.com>
2023-01-19 18:28:52 -0600

    refactor state on boardDetail and NewPost to rerender on submit

--- inspo-us/src/components/BoardDetails.js
+++ inspo-us/src/components/BoardDetails.js
@@ -11,8 +11,35 @@ export default function BoardDetails() {
     const [loading, setLoading] = useState(true);
     const { id, boardId } = useParams();
     const [post, setPost] = useState();
-
+    const [state, setState] = useState({
+        img_url: "",
+        body: ""
+      })
   
+
+    const handleChange = e => {
+        setState({
+            ...state,
+            [e.target.name]: e.target.value
+          })
+      }
+
+      const handleSubmit = async e => {
+        e.preventDefault();
+        const headers = {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        }
+        // const body = { img_url: imgUrl,
+        // body: postBody }
+        const options = {
+          method: "POST",
+          headers,
+          body: JSON.stringify(state)
+        }
+        await fetch(`http://localhost:9292/users/${id}/boards/${boardId}/posts`, options)
+    
+    }
     
 
     useEffect(() => {
@@ -25,7 +52,7 @@ export default function BoardDetails() {
           setPost(data.posts)
         }
         loadPosts();
-    }, [boardId, id])
+    }, [boardId, id, handleSubmit])
     
   
       if(loading) {
@@ -37,7 +64,7 @@ export default function BoardDetails() {
   return (
     <div>
       <h1>{posts.title}</h1>
-      < NewPost id={id} boardId={boardId} />
+      < NewPost handleChange={handleChange} handleSubmit={handleSubmit} state={state}/>
       { sortedPosts.map(post => <PostCard key={post.id} post={post} />)}
     </div>
   )
